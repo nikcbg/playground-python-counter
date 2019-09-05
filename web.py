5d77238447e17b1a6efd578e595c9a575d1772e6
nikcbg <43785382+nikcbg@users.noreply.github.com>
2019-09-05 13:21:28 +0300

    Create web.py

---
+++ web.py
@@ -0,0 +1,9 @@
+from flask import Flask
+
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return 'Page visits: 0'
+if __name__ == '__main__':
+    app.run(debug=True, port=8081, host='0.0.0.0')
+
