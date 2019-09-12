a6c3ee66dce2dcd0dcf01c6ca6d06993ed563610
nikcbg <43785382+nikcbg@users.noreply.github.com>
2019-09-12 16:02:00 +0300

    Update db.py

--- db.py
+++ db.py
@@ -3,30 +3,25 @@
 import os
 import redis
 
-
 redis_host = "127.0.0.1"
 redis_port = 6379
 redis_password = ""
 
-def connect():
-    
+def mycounter():
     # create the Redis Connection object
     try:
-
         r = redis.StrictRedis(host=redis_host, port=redis_port,password=redis_password, decode_responses=True)
     
-        # Set the connection message in Redis 
-        r.set("msg:connect", "Connection established!!!")
-
-        # Retrieve the connection message from Redis
-        msg = r.get("msg:connect")
-        print(msg)        
+        counter = r.incr("mydb")
+        return(counter)        
     
     except Exception as e:
         print(e)
 
 if __name__ == '__main__':
-    connect()
+    counter = mycounter()
+    print(counter)
+
 
 
 
